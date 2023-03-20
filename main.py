e87eb22b9c32d117825dc0bf0c4e643d92d0f0e3
Aaron <aaron.katz.mobile@gmail.com>
2023-03-20 18:12:13 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,50 @@
+# This is day 3 of 100 in the Python Bootcamp
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+path = input("The path you've followed ends at a crossroad, do you go left or right? ")
+if path == "left":
+    path = input("You took the path to the left and came to a small lake. Do you swim across or walk around it? ")
+else:
+    print("You went to the right and fell into a hole that a hunter had dug to trap the weary...Game Over.")
+
+if path == "walk":
+    path = input("You walked around the lake, a shack is on its far shore. Inside are 3 doors: red, blue or yellow. ")
+    if path == "yellow":
+        print("You found the treasure! You Win!")
+    elif path == "red":
+        print("You opened the red door, triggering a trap that casts fireball...Game Over")
+    else:
+        print("You opened the blue door were eaten by ghouls...Game Over")
+else:
+    print("You tried to swim across, but were devoured by hungry piranha...Game Over.")
+
+
+
+
+
